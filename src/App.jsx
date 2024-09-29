108d391820156b308a3bbd98d6b07a131576cacf
Rika Kamila <rikakamila1717@gmail.com>
2024-09-29 21:26:30 +0700

    Add Cartlist , API n import to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+
 import Navbar from './components/Navbar'
+import CartList from './components/CartList'
 import './index.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className='app'>
       <Navbar />  
+      <CartList  />
 
     </div>
   )
