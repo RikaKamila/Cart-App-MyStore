7044216c2145eb13395b190daccedc5e9af28692
Rika Kamila <rikakamila1717@gmail.com>
2024-09-29 21:34:21 +0700

    Add component counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,6 @@
 import React, { useState } from 'react'
 
 function Counter({onCountChange}) {
-  const [count, setCount] = useState(0)
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-    onCountChange(count + 1); 
-  };
-
-  const handleDecrement = () => {
-    if (count > 0) {
-      setCount(count - 1);
-      onCountChange(count - 1);
-    }
-  };
 
   return (
     <div>
