49ed33e517bdea2857ef4d80877112fcb22779db
Rika Kamila <rikakamila1717@gmail.com>
2024-09-29 21:36:39 +0700

    Add component footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
-
-
 function Footer() {
   return (
-    <div className=" bg-teal-700">
-    <div className="p-3 py-5 container mx-auto text-center">
-        <span className="text-white text-xl ">&copy; 2024 Cart App Using React By Rika. All rights reserved.</span>
-    </div>
-    </div>
 
   );
 }
