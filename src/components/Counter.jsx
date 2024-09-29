630970c44592e3115eb0ba12516953e620e7ca22
Rika Kamila <rikakamila1717@gmail.com>
2024-09-29 21:35:20 +0700

    Add State onCountChange

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,19 @@
 import React, { useState } from 'react'
 
 function Counter({onCountChange}) {
+    const [count, setCount] = useState(0)
+
+    const handleIncrement = () => {
+      setCount(count + 1);
+      onCountChange(count + 1); 
+    };
+  
+    const handleDecrement = () => {
+      if (count > 0) {
+        setCount(count - 1);
+        onCountChange(count - 1);
+      }
+    };
 
   return (
     <div>
