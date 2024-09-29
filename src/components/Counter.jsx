9d4af3dd374da32c24e6198866e6489ef6a9f1f8
Rika Kamila <rikakamila1717@gmail.com>
2024-09-29 21:51:52 +0700

    Add redux

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { cartIncrease, cartDecrease } from '../redux/cartSlice'
 
 function Counter({onCountChange}) {
+    const dispatch = useDispatch()
     const [count, setCount] = useState(0)
 
     const handleIncrement = () => {
       setCount(count + 1);
       onCountChange(count + 1); 
+      dispatch(cartIncrease())
     };
   
     const handleDecrement = () => {
       if (count > 0) {
         setCount(count - 1);
         onCountChange(count - 1);
+        dispatch(cartDecrease())
       }
     };
 
