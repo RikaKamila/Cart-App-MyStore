a1a85c1470e488ce9e5e1d4e25b739b350e7f4f4
Rika Kamila <rikakamila1717@gmail.com>
2024-09-29 21:38:43 +0700

    Complete App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-
 import Navbar from './components/Navbar'
 import CartList from './components/CartList'
+import Footer from './components/Footer'
 import './index.css'
 
 function App() {
-
 
   return (
     <div className='app'>
       <Navbar />  
       <CartList  />
+      <Footer />
 
     </div>
   )
